c780057226bb129ff91d4d81ef2a1dbc0c818c22
Mkhitar <60759098+MkhitarSiradeghyan@users.noreply.github.com>
2023-12-10 04:58:10 +0400

    preload css 3

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,36 +5,39 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-EL6QKKQH7C" />
-      <Script id="google-analytics">
-        {`
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-EL6QKKQH7C" />
+        <Script id="google-analytics">
+          {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
  
           gtag('config', 'G-EL6QKKQH7C');
         `}
-      </Script>
-        
-        <meta name="description" content="This is the official website of ArmWay Tour company"/>
-        <title>ArmWay Tour</title>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
+        </Script>
+
+        <meta
+          name="description"
+          content="This is the official website of ArmWay Tour company"
+        />
         <link
-        async
+          async
           rel="stylesheet"
           type="text/css"
           charSet="UTF-8"
           href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
         />
         <link
-        async
+          async
           rel="stylesheet"
           type="text/css"
           href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
         />
+        <title>ArmWay Tour</title>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
       </body>
     </Html>
   );
